982e6473b4db74cbb620364f2ceb6271730db8e5
Mona Lisa <divijmathur@gmail.com>
2020-01-29 18:52:05 -0800

    segmenting exercises

--- Roomba/roombaInstructions.js
+++ Roomba/roombaInstructions.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-// function describing the notation for specific directions and what the roomba should do when in these specific situations
+// function describing the notation for specific directions and what the roomba should do when givin these specific directions
 function runInstructions(cardinalDirection) {
   switch (cardinalDirection) {
     case 'N':
@@ -20,9 +20,9 @@ function runInstructions(cardinalDirection) {
   }
 }
 
+// we're checking here what patch has been cleaned by our roomba
 function dirtCheck(){
   if(patchOfDirt.indexOf(currCoordinates.join(' ')) > -1 ){
-    console.log(`Patch cleaned at position (${currCoordinates})`);
     cleanPatchCounter += 1;
     // to delete the patch of dirt that has already been cleaned by our roomba
     patchOfDirt.splice(patchOfDirt.indexOf(currCoordinates.join(' '),1));
@@ -37,17 +37,17 @@ function touchingWall(){
   currCoordinates[1] === dimensionOfRoom[1] && cardinalDirections[i]==='N'); 
 }
 
-// counter to keep track of which patches of dirt are clean
+// counter to keep track of which patches of dirt are clean 
 var cleanPatchCounter = 0;
 
 // take input from text file and convert it to a string
 var text = fs.readFileSync('input.txt').toString('utf-8');
 var fileContent = text.split("\n");
 
-// index of room dimensions that have been given to us
+// split the room dimensions that have been given to us
 var dimensionOfRoom= fileContent[0].split(' ');
 
-// starting point of roomba that is given to us
+// split the starting point of roomba that is given to us
 var currCoordinates = fileContent[1].split(' ');
 
 // the index of the cardinal directions given to us
@@ -70,6 +70,6 @@ for(var i = 0; i < cardinalDirections.length;i++){
     console.log(`Hit wall at the position (${currCoordinates}) and cardinal Direction (${cardinalDirections[i]})`);
   }
 }
-
+// we'll output the final position of the roomba after it is done cleaning the patches and also output the number of dirt patches it cleaned on the console.
 console.log(`The final Position of the Roomba is (${currCoordinates})`);
 console.log(`The number of patches that have been cleaned is (${cleanPatchCounter})`);
